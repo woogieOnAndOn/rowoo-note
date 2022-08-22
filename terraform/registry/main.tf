6b758a53fd67423b54764c8697881919aac7e571
woogieOnAndOn <123wodnr@naver.com>
2022-08-22 15:28:13 +0900

    [terraform] registry arg 변경

--- terraform/registry/main.tf
+++ terraform/registry/main.tf
@@ -20,13 +20,14 @@ provider "aws" {
 
 resource "aws_ecr_repository" "repository" {
   name                 = var.registry_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE" // Default: MUTABLE, 동일한 태그를 사용하는 후속 이미지 푸시가 이미지 태그를 덮어쓰지 않도록 방지(IMMUTABLE)
+
   tags                 = {
     Name = var.registry_name
   }
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false // 사용 중단 경고, 리포지토리 수준의 ScanOnPush 구성은 더 이상 사용되지 않으며 레지스트리 수준 스캔 필터로 대체
   }
 }
 
