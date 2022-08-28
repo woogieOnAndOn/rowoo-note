a7523b29d6d12d2f033b084947af0b84a3d53ffe
woogieOnAndOn <123wodnr@naver.com>
2022-08-28 20:53:18 +0900

    [terraform] aws_ecs_service.network_configuration.assign_public_ip false > true 변경

--- terraform/deployment/service.tf
+++ terraform/deployment/service.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_service" "ecs_service" {
 
   network_configuration {
     subnets          = each.value
-    assign_public_ip = false
+    assign_public_ip = true
     security_groups  = [aws_security_group.ecs_tasks.id]
   }
 }
