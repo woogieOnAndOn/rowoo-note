6c13b703cb28ec6e0a60d8bcfc30a6fdf49b141e
woogieOnAndOn <123wodnr@naver.com>
2022-08-28 20:34:38 +0900

    [terraform] aws_ecs_service.desired_count 2 > 1 로 원복

--- terraform/deployment/service.tf
+++ terraform/deployment/service.tf
@@ -30,7 +30,7 @@ resource "aws_ecs_service" "ecs_service" {
   name            = "${var.service_name}-service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.service_task_fargate.arn
-  desired_count   = 2
+  desired_count   = 1
   launch_type     = "FARGATE"
   for_each        = toset(data.aws_subnets.public.ids)
 
