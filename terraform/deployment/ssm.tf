15e8a38d15caf3c6238f7a45237c10f564aa828e
woogieOnAndOn <123wodnr@naver.com>
2022-09-04 19:22:30 +0900

    [terraform] ssm resource명 변경

--- terraform/deployment/ssm.tf
+++ terraform/deployment/ssm.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "ecs-task-ssm" {
+resource "aws_ssm_parameter" "ecs_task_ssm" {
   name  = "${var.service_name}-ecs-task-ssm"
   type  = "String"
   value = "${data.aws_caller_identity.current.account_id}-ssm"
