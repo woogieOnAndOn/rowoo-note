2b8cc72d1f44f8ad0356a892a30d9adf5d39a0b3
woogieOnAndOn <123wodnr@naver.com>
2022-09-04 19:55:40 +0900

    [terraform] ecs task execution 원복

--- terraform/deployment/iam.tf
+++ terraform/deployment/iam.tf
@@ -1,22 +1,16 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${var.service_name}-ecs-task-execution-role"
   assume_role_policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
+    Version = "2012-10-17"
+    Statement = [
       {
-        Effect : "Allow",
-        Action : [
-          "ssm:GetParameters",
-          "secretsmanager:GetSecretValue",
-          "kms:Decrypt"
-        ],
-        Resource : [
-          "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/${aws_ssm_parameter.ecs_task_ssm.name}",
-          "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:${aws_secretsmanager_secret.ecs_task_secret_manager.name}",
-          "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:key/${aws_kms_key.current.key_id}"
-        ]
-      }
-    ]
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Sid    = "",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+    }]
   })
 }
 
