e6c9716f428b46f995215d945178355ead64c868
woogieOnAndOn <123wodnr@naver.com>
2022-09-04 21:13:27 +0900

    [terraform] ssm, secret manager 삭제

--- terraform/deployment/secretManager.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_secretsmanager_secret" "ecs_task_secret_manager" {
-  name = "${var.service_name}-ecs-secret-manager"
-}
--- terraform/deployment/ssm.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "ecs_task_ssm" {
-  name  = "${var.service_name}-ecs-task-ssm"
-  type  = "String"
-  value = "${data.aws_caller_identity.current.account_id}-ssm"
-}
